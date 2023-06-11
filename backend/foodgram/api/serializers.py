d8995b5252aff35d6ff9e5fe920776572c45bec7
Gelya <gelyadoma@gmail.com>
2023-06-11 21:45:44 +0300

    Уникальность ингредиентов и дебаг

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework import status
 from rest_framework.validators import UniqueTogetherValidator
 
 from users.models import Follow
